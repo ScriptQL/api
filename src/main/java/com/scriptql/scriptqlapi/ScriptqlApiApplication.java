d0857b288ea4eb18687949de6d2a827299807482
Tiago Valdrich <tiago.valdrich@hotmail.com>
2022-03-24 18:30:25 -0300

    chore: initial project

---
+++ src/main/java/com/scriptql/scriptqlapi/ScriptqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.scriptql.scriptqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptqlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scriptql/scriptqlapi/ScriptqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptql.scriptqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
